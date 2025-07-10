5ac17e4539b45df74087b672d512f94f1cbebe71
jiyahargun <jiyahargun82@gmail.com>
2025-07-10 10:19:22 +0530

    Add files via upload

---
+++ c++/09-07-2025/Addition.cpp.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    
+    int num1,num2,ans;
+    
+    cout<<"Enter a number1 :";
+    cin>>num1;
+    
+    cout<<"Enter a number2 :";
+    cin>>num2;
+    
+    ans = num1+num2;
+    cout<<"Your Answer is"<<ans;
+    
+    
+    return 0;
+    
+}
---
+++ c++/09-07-2025/Hello.cpp.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"hello jiya"<<endl;
+    cout<<"jetpur"<<endl;
+    
+    return 0;
+}
